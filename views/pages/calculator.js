eff21f6a0fa4993ce008160fd17f5198b3d18d23
汪瀚堃 <wanghankun@wanghankundeMacBook-Air.local>
2022-01-30 17:09:56 -0800

    asn1

---
+++ views/pages/calculator.js
@@ -0,0 +1,9 @@
+
+function comput_percent(num_id, denom_id ){
+    var num = document.getElementById(num_id).innerHTML;
+    var denom = document.getElementById(denom_id).innerHTML;
+    return num/denom
+}
+
+
+
